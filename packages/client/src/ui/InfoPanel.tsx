da5ceb999713e7494274ceacc09cf0b9faad8788
TAK_Ryuji <kozayupapa@gmail.com>
2023-05-23 06:14:25 +0900

    add top3 point

--- packages/client/src/ui/InfoPanel.tsx
+++ packages/client/src/ui/InfoPanel.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { twMerge } from "tailwind-merge";
-import { useComponentValue } from "@latticexyz/react";
+import { useComponentValue,useEntityQuery } from "@latticexyz/react";
+import { Has, HasValue, getComponentValueStrict } from "@latticexyz/recs";
 import { useMUD } from "../store";
 
 export const InfoPanel = () => {
@@ -15,16 +16,29 @@ export const InfoPanel = () => {
   const player = useComponentValue(Player,playerEntity);
   console.log(player);
 
+  const entities = useEntityQuery([Has(Player)]);
+  console.log(entities);
+  const players =   entities.map(entity => getComponentValueStrict(Player, entity));
+  console.log(players);
+  players.sort((a,b)=>Number(b.last - a.last));
+  const top3 = players.slice(0,3);
+
+  
   return (
     <div
-    className={twMerge( 
-      "flex flex-col gap-10 items-center justify-center bg-black text-white transition-opacity duration-1000",
-      appear ? "opacity-100" : "opacity-0"
-    )}
-  >
-    <div className="text-8xl animate-bounce">{monsterEmoji}</div>
-    <div>Player point: {player ? player.last.toString() : 0}</div>      
-  </div>    
+      className={twMerge( 
+        "flex flex-col gap-10 items-center justify-center bg-black text-white transition-opacity duration-1000",
+        appear ? "opacity-100" : "opacity-0"
+      )}
+    >
+      <div className="text-8xl animate-bounce">{monsterEmoji}</div>
+      <div>Player point: {player ? player.last.toString() : 0}</div>      
+      <ul>
+      {players.map(playerp => (
+        <li>{playerp.last.toString()} {playerp.x} {playerp.y} <br/></li>
+      ))}
+      </ul>  
+    </div>    
   );
 };
 
